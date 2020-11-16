687384ae27d3436ee406c124878c3dfcab87682a
stuart <stuartsemail@yahoo.com>
2020-11-16 20:37:52 +0000

    added react router to menu-item component

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title,id,imageUrl,size}) => (
+const MenuItem = ({title,id,imageUrl,size,history,linkUrl,match}) => (
 
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}
+     onClick={()=>history.push(`${match.url}${linkUrl}`)}>
       <div className="background-image"
       style={{backgroundImage:`url(${imageUrl})`}}></div>
     <div className='content'>
@@ -13,9 +15,10 @@ const MenuItem = ({title,id,imageUrl,size}) => (
     </div>
   </div>
 
+  
 
 
 
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
